a0835d3c0a5ec42548c0c27c1119b791bd479ab5
admin <admin@10.5.132.34>
2018-12-05 20:00:49 +0800

    IU-2018.2.1 <admin@XLFU Create markdown.xml, vim_settings.xml, ide.general.xml, git.xml, CodeGlance.xml, project.default.xml, rainbow_brackets.xml, Default copy.xml, lombok-plugin.xml, Default.xml, Default-20181017.icls, other.xml, yiiguxing.translation.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, SQL Script.sql, File Header.java, filetypes.xml, projectView.xml, qaPlugPluginUpdateNotifier.xml, keymap.xml, plugin_ui.xml, colors.scheme.xml, Default copy 1.xml, databaseDrivers.xml, mavenVersion.xml, templates.xml, Default.xml, laf.xml, KeyPromoterXStatistic.xml, jdk.table.xml, debugger.renderers.xml, path.macros.xml, mavenRunHelper.xml, Custom Postfix Templates.xml, web-browsers.xml, customPostfixTemplates.xml, vcs.xml, editor.codeinsight.xml, grepConsole.xml, toStringTemplates.xml, editor.xml, usageView.xml, debugger.xml, java.decompiler.xml, sonarlint.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,13 @@
+/**
+ * ${PROJECT_NAME}.java Create on ${DATE}
+ * <p>
+ * Copyright (c) ${DATE}   All Rights Reserved By 知学科技
+ *
+ * @Description: 
+ * <p>
+ * -----------------------------ChangeLog---------------------------------
+ *
+ * @author xlfu   @version   V1.0   @Date  ${DATE} ${TIME}
+ * 初始化创建
+ *
+ */
